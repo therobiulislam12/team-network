4f48f309cf5af117d2beebf49e1d7d463b1eb26c
Robiul Islam <therobiulislam12@gmail.com>
2024-10-19 16:43:41 +0600

    elementor addon setup done

--- includes/plugin.php
+++ includes/plugin.php
@@ -1,1 +1,133 @@
-<?php 
+<?php
+
+namespace ElementorTeamNetwork;
+
+/**
+ * Class Plugin
+ *
+ * Main Plugin class
+ * @since 1.0.0
+ */
+class Plugin {
+
+    /**
+     * Instance
+     *
+     * @since 1.0.0
+     * @access private
+     * @static
+     *
+     * @var Plugin The single instance of the class.
+     */
+    private static $_instance = null;
+
+    /**
+     * Instance
+     *
+     * Ensures only one instance of the class is loaded or can be loaded.
+     *
+     * @since 1.0.0
+     * @access public
+     *
+     * @return Plugin An instance of the class.
+     */
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * widget_scripts
+     *
+     * Load required plugin core files.
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function widget_scripts() {
+        wp_register_script( 'elementor-hello-world', plugins_url( '/assets/js/hello-world.js', __FILE__ ), ['jquery'], false, true );
+    }
+
+    /**
+     * Register Widgets
+     *
+     * Register new Elementor widgets.
+     *
+     * @since 1.0.0
+     * @access public
+     *
+     * @param Widgets_Manager $widgets_manager Elementor widgets manager.
+     */
+    public function register_widgets( $widgets_manager ) {
+        // Its is now safe to include Widgets files
+        // require_once __DIR__ . '/widgets/hello-world.php';
+        // require_once __DIR__ . '/widgets/inline-editing.php';
+        require_once __DIR__ . '/widgets/test.php';
+
+        // Register Widgets
+        // $widgets_manager->register( new Widgets\Hello_World() );
+        // $widgets_manager->register( new Widgets\Inline_Editing() );
+        $widgets_manager->register( new Widgets\Test_addons() );
+    }
+
+    /**
+     * Add new MegaKit Core Widgets Category
+     * @param mixed $elements_manager
+     * @return void
+     */
+    public function team_network_widget_category( $elements_manager ) {
+
+        $categories = [];
+        $categories['team-network'] = [
+            'title'       => 'Team Network',
+            'icon'        => 'fa fa-plug',
+            'hideIfEmpty' => true,
+        ];
+
+        // Get existing categories and merge them with the new one
+        $old_categories = $elements_manager->get_categories();
+        $categories = array_merge( $categories, $old_categories );
+
+        // var_dump( $categories );exit();
+
+        // Check if there's a public method to set categories
+        if ( method_exists( $elements_manager, 'set_categories' ) ) {
+            // If the method exists, set the categories
+            $elements_manager->set_categories( $categories );
+        } else {
+            // Handle cases where there is no public setter for categories
+            $set_categories = function ( $categories ) {
+                $this->categories = $categories;
+            };
+
+            // Use Closure to set categories in the elements manager
+            $set_categories->call( $elements_manager, $categories );
+        }
+    }
+
+    /**
+     *  Plugin class constructor
+     *
+     * Register plugin action hooks and filters
+     *
+     * @since 1.0.0
+     * @access public
+     */
+    public function __construct() {
+
+        // register widgets category
+        add_action('elementor/elements/categories_registered', array($this, 'team_network_widget_category'));
+
+        // Register widget scripts
+        add_action( 'elementor/frontend/after_register_scripts', [$this, 'widget_scripts'] );
+
+        // Register widgets
+        add_action( 'elementor/widgets/register', [$this, 'register_widgets'] );
+
+    }
+}
+
+// Instantiate Plugin Class
+Plugin::instance();
---
+++ includes/widgets/test.php
@@ -0,0 +1,147 @@
+<?php
+namespace ElementorTeamNetwork\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Test Addons
+ *
+ * Elementor widget for Portfolio Elementor Kit.
+ *
+ * @since 1.0.0
+ */
+class Test_addons extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'test';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Test Buddy', 'team-network' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return 'eicon-posts-ticker';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'team-network' ];
+	}
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => __( 'Content', 'team-network' ),
+			]
+		);
+
+		$this->add_control(
+			'title',
+			[
+				'label' => __( 'Title', 'team-network' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Style', 'team-network' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->add_control(
+			'text_transform',
+			[
+				'label' => __( 'Text Transform', 'team-network' ),
+				'type' => Controls_Manager::SELECT,
+				'default' => '',
+				'options' => [
+					'' => __( 'None', 'team-network' ),
+					'uppercase' => __( 'UPPERCASE', 'team-network' ),
+					'lowercase' => __( 'lowercase', 'team-network' ),
+					'capitalize' => __( 'Capitalize', 'team-network' ),
+				],
+				'selectors' => [
+					'{{WRAPPER}} .title' => 'text-transform: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+
+		echo '<div class="title">';
+		echo $settings['title'];
+		echo '</div>';
+	}
+}
